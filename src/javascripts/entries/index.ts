8eac42651130a8963694544e000e5c68d97acb73
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-05-10 16:02:01 +0900

    vue-fontawesomeの導入

--- src/javascripts/entries/index.ts
+++ src/javascripts/entries/index.ts
@@ -1,6 +1,13 @@
 import "@css/index.scss";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import Vue from "vue";
 
+library.add(fas, fab, far);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 new Vue({
   el: "#vue-root",
   components: {},
